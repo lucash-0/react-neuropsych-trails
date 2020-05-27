aab23e549f23923be323d5488c1817c6b605b586
lucash-12554200 <55736681+lucash@users.noreply.github.com>
2020-05-27 19:45:37 +1000

    added randomisation to trails_a_12

--- src/trails_a_12.jsx
+++ src/trails_a_12.jsx
@@ -1,44 +1,39 @@
-export default function trails() {
-  const xlowest = 38;
-  const xhighest = 394;
-  const ylowest = 36;
-  const yhighest = 418;
+const d = 44; //diameter
+
+function tokens() {
+  const xlowest = 50;
+  const xhighest = 710;
+  const ylowest = 50;
+  const yhighest = 510;
+  const d2 = d * 2;
 
   let tokens = [];
   let xrange = [];
   let yrange = [];
 
-  let i = 0;
-  while (i < 12) {
-    const x = xlowest + Math.random() * (xhighest - xlowest);
-
-    const xcheck = xrange.every((xr) => {
-      if (x < xr && x + 44 - xr < 0) return true;
-      if (x > xr + 44) return true;
-
-      return false;
-    });
-
-    if (xcheck) {
-      xrange.push(x);
-      i++;
-    }
+  for (let i = 0; i < 12; i++) {
+    const x = Math.round(xlowest + Math.random() * (xhighest - xlowest));
+    xrange.push(x);
   }
 
-  let j = 0;
-  while (j < 12) {
-    const y = ylowest + Math.random() * (yhighest - ylowest);
+  let i = 0;
+  while (i < 12) {
+    const y = Math.round(ylowest + Math.random() * (yhighest - ylowest));
 
-    const ycheck = yrange.every((yr) => {
-      if (y < yr && y + 44 - yr < 0) return true;
-      if (y > yr + 44) return true;
+    const ycheck = yrange.every((yr, index) => {
+      if (!(y + d2 - yr < 0 || yr + d2 - y < 0)) {
+        return (
+          xrange[i] + d2 - xrange[index] < 0 ||
+          xrange[index] + d2 - xrange[i] < 0
+        );
+      }
 
-      return false;
+      return true;
     });
 
     if (ycheck) {
       yrange.push(y);
-      j++;
+      i++;
     }
   }
 
@@ -46,14 +41,16 @@ export default function trails() {
     tokens.push({
       x: xrange[k],
       y: yrange[k],
-      text: k + 1,
+      text: "" + (k + 1),
     });
   }
-
-  return {
-    width: 535,
-    height: 465,
-    diameter: 44,
-    tokens: tokens,
-  };
+  console.log(tokens);
+  return tokens;
 }
+
+export default {
+  width: 758,
+  height: 558,
+  diameter: d,
+  tokens: tokens(),
+};
